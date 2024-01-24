b8ca071db467442fa18b810f200a40d6c6bce69e
Mohd Zain <zainmohd1998@gmail.com>
2024-01-24 20:15:39 +0530

    fix:localhost db

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const mongoUri = REACT_APP_DB
+// const mongoUri = process.env.REACT_APP_DB
+const mongoUri = "mongodb://localhost:27017"
+
 const connectToMongo = async () =>{
     try{
         await mongoose.connect(mongoUri)
