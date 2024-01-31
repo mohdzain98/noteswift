d226d7796593d1d425c62d86df6a62c5841d02bb
Mohd Zain <zainmohd1998@gmail.com>
2024-01-31 11:22:56 +0530

    Fix: navbar collapse on mobile devices

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" to='/' onClick={rollNavBack}>NoteSwift</Link>
+                <Link className="navbar-brand" to='/'>NoteSwift</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" ref={ref}>
                 <span className="navbar-toggler-icon"></span>
                 </button>
